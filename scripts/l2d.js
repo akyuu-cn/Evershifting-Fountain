123057a1c7fc7e6bc4a9e24ccdd1989b1612b031
ltfjx <ltfjx2333@gmail.com>
2022-06-16 16:49:41 +0800

    thermal cat :D

---
+++ scripts/l2d.js
@@ -0,0 +1,3 @@
+hexo.extend.filter.register('theme_inject', function(injects) {
+    injects.footer.raw('default', '<script src="https://ltfjx-hexo-blog-1256621903.cos.ap-shanghai.myqcloud.com/live2d-widget-master/autoload.js"></script>');
+  });
