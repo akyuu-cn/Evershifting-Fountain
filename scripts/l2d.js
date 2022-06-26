de29ae2f1e198acc1498babbcd4a4a92454f0f93
ltfjx <ltfjx2333@gmail.com>
2022-06-26 10:50:50 +0800

    升级主题&修改目录结构&修改l2d注入方式

--- scripts/l2d.js
+++ scripts/l2d.js
@@ -1,3 +1,1 @@
-hexo.extend.filter.register('theme_inject', function(injects) {
-    injects.footer.raw('default', '<script src="https://ltfjx-hexo-blog-1256621903.cos.ap-shanghai.myqcloud.com/live2d-widget-master/autoload.js"></script>');
-  });
+hexo.extend.injector.register('head_begin', '<script src="https://ltfjx-hexo-blog-1256621903.cos.ap-shanghai.myqcloud.com/live2d-widget-master/autoload.js"></script>', 'default');
