5011d1e1ea32bb9b2ed766f5fcd8e3f8a668cac4
Ltfjx <ltfjx2333@gmail.com>
2022-07-21 00:50:05 +0800

    rua

--- scripts/potato.js
+++ scripts/potato.js
@@ -2,5 +2,3 @@ hexo.extend.injector.register('body_end', '<script src="/potato-resources/potato
 hexo.extend.injector.register('body_end', '<script src="/potato-resources/potatojs/cate_sort.js"></script>', 'default');
 hexo.extend.injector.register('body_end', '<script src="/potato-resources/potatojs/tag_sort.js"></script>', 'default');
 hexo.extend.injector.register('body_end', '<script src="/potato-resources/potatojs/remove_h2_line.js"></script>', 'default');
-hexo.extend.injector.register('body_end', '<script src="/potato-resources/potatojs/index_banner.js"></script>', 'default');
-hexo.extend.injector.register('head_begin', '<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">', 'default');
--- source/potato-resources/potatojs/index_banner.js
+++
@@ -1,7 +0,0 @@
-
-if(location.pathname === "/"){
-    document.getElementById("BannerText").style.top = (banner.offsetHeight/2)*0.95 + "px"
-}else{
-document.getElementById("BannerText").style.top = (banner.offsetHeight/2)*0.8 + "px"}
-
-document.getElementById("BannerText").style.opacity=1
