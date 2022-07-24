d35d72a0f77b4a27b5a25de49e98b76f0c7c8b25
Ltfjx <ltfjx2333@gmail.com>
2022-07-24 23:59:15 +0800

    使用了 @lingrottin 提供的 parallax 方案，优化移动端标题显示

--- scripts/potato.js
+++ scripts/potato.js
@@ -2,3 +2,4 @@ hexo.extend.injector.register('body_end', '<script src="/potato-resources/potato
 hexo.extend.injector.register('body_end', '<script src="/potato-resources/potatojs/cate_sort.js"></script>', 'default');
 hexo.extend.injector.register('body_end', '<script src="/potato-resources/potatojs/tag_sort.js"></script>', 'default');
 hexo.extend.injector.register('body_end', '<script src="/potato-resources/potatojs/remove_h2_line.js"></script>', 'default');
+hexo.extend.injector.register('body_end', '<script src="/potato-resources/potatojs/mobile_navbar_reducement.js"></script>', 'default');
